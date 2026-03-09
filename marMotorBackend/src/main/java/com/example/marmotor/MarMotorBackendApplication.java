91fe8a479372fc330ffa592b3b616ed8fa22783b
Oscar Acedo <oscaracedonis@gmail.com>
2026-03-09 09:42:11 +0100

    Creacion del proyecto

---
+++ marMotorBackend/src/main/java/com/example/marmotor/MarMotorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.marmotor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarMotorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarMotorBackendApplication.class, args);
+    }
+
+}
