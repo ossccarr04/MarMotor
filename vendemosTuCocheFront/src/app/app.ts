875fb74b26e69663cbd242e7451da12952c1f06b
Oscar Acedo <oscaracedonis@gmail.com>
2026-03-09 10:33:51 +0100

    Refactorizado la estructura del proyecto y actualizado las configuraciones

--- vendemosTuCocheFront/src/app/app.ts
+++ vendemosTuCocheFront/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('vendemosTuCocheFront');
+  protected readonly title = signal('marMotor');
 }
