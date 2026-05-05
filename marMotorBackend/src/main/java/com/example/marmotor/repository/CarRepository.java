844a9a58e85aec58246858509647cab783d537d5
Oscar Acedo <oscaracedonis@gmail.com>
2026-05-05 08:58:41 +0200

    Arreglo eliminar coches por favoritos

--- marMotorBackend/src/main/java/com/example/marmotor/repository/CarRepository.java
+++ marMotorBackend/src/main/java/com/example/marmotor/repository/CarRepository.java
@@ -3,6 +3,7 @@ package com.example.marmotor.repository;
 import com.example.marmotor.model.entity.Car;
 import com.example.marmotor.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,5 +21,9 @@ public interface CarRepository extends JpaRepository<Car, Long> {
             "WHERE LOWER(CONCAT(b.name, ' ', c.model)) LIKE LOWER(CONCAT('%', :query, '%'))",
             nativeQuery = true)
     List<Car> findByFullSearch(@Param("query") String query);
+
+    @Modifying
+    @Query(value = "DELETE FROM favorites WHERE car_id = :carId", nativeQuery = true)
+    void deleteCarFromAllFavorites(@Param("carId") Long carId);
 
 }
