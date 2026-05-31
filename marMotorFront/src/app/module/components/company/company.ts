d11f810fbeb879df2f53f62f59d8b92a58bf0cbc
Oscar Acedo <oscaracedonis@gmail.com>
2026-05-31 23:39:57 +0200

    correccion de correo

--- marMotorFront/src/app/module/components/company/company.ts
+++ marMotorFront/src/app/module/components/company/company.ts
@@ -17,22 +17,4 @@ export class Company {
     this.emailContact = environment.EMAIL_CONTACT;
     this.numberContact = environment.NUMBER_CONTACT;
   }
-
-  abrirYCopiarCorreo(email: String) {
-    const correoText = email.toString();
-
-    // 1. Copiamos al portapapeles y avisamos al usuario (puedes cambiar el alert por un Toast más adelante)
-    navigator.clipboard.writeText(correoText).then(() => {
-      alert('¡Correo copiado al portapapeles: ' + correoText + '!\nAbriendo gestor de correo...');
-    });
-
-    // 2. El truco definitivo: Crear un enlace "fantasma" en memoria y forzar el clic
-    const mailtoLink = document.createElement('a');
-    mailtoLink.href = `mailto:${correoText}`;
-
-    // Lo añadimos al documento, lo clicamos y lo destruimos rápidamente
-    document.body.appendChild(mailtoLink);
-    mailtoLink.click();
-    document.body.removeChild(mailtoLink);
-  }
 }
